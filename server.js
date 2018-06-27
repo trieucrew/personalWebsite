980e68ef61f59b2ff291037d975509222d891241
Jason Trieu <j2trieu@ucsd.edu>
2018-06-27 12:51:52 -0700

    updated

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-consst express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/views', (req, res) => {
+  res.send('hi');
+});
+
 app.listen(3000, () => {
   console.log('Server started');
-})
+});
